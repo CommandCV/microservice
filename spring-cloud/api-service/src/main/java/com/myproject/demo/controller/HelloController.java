16c5ede473da6948c7b14a227fd1ea9c8b610773
sunsongyang <707509803@qq.com>
2023-11-09 21:16:36 +0800

    feat(api-service): add api-service module and hello controller

---
+++ spring-cloud/api-service/src/main/java/com/myproject/demo/controller/HelloController.java
@@ -0,0 +1,17 @@
+package com.myproject.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping(value = "/")
+@RestController
+public class HelloController {
+
+    @GetMapping(value = "/hello")
+    public String hello(@RequestParam String name) {
+        return "hello " + name;
+    }
+
+}
